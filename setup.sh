9a82c546511472f2b0a1cbcb4afd904e35a9ce1b
Jack-0 <github.ay8yf@slmail.me>
2023-07-24 19:12:43 +0100

    added a setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 DOTFILE_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-# add scripts to users account
-ln -s $DOTFILE_DIR/scripts/* $HOME/.local/bin
+
+# SCRIPTS --------------------------------------------------------------------
+SCRIPTS_DIR="$HOME/.local/bin"
+[ -d $SCRIPTS_DIR ] || mkdir $SCRIPTS_DIR
+ln -s $DOTFILE_DIR/scripts/* $SCRIPTS_DIR
 
 # delete any broken symbolic links
 #find -L $HOME/.local/bin -type l -exec rm {}\;
@@ -11,10 +14,25 @@ ln -s $X/scripts/* $HOME/.local/bin
 for file in "$DOTFILE_DIR/scripts/"*; do
     if [ -f "$file" ] && [ -x "$file" ]; then
         filename=$(basename "$file")
-        if [ -e "$HOME/.local/bin/$filename" ]; then
-            echo "chmod +x $HOME/.local/bin/$filename"
-            chmod +x "$HOME/.local/bin/$filename"
+        if [ -e "$SCRIPTS_DIR/$filename" ]; then
+            echo "chmod +x $SCRIPTS_DIR/$filename"
+            chmod +x "$SCRIPTS_DIR/$filename"
         fi
     fi
 done
 
+# ZSH ------------------------------------------------------------------------
+ln -s $DOTFILE_DIR/zsh/zshrc $HOME/.zshrc
+
+# create ZSH_CONFIG_DIR if no dir
+ZSH_CONFIG_DIR="$HOME/.config/zsh"
+[ -d $ZSH_CONFIG_DIR ] || mkdir $ZSH_CONFIG_DIR
+
+# symbolic link zsh files
+for file in "$DOTFILE_DIR/zsh/"*; do
+    if [ $file != "$DOTFILE_DIR/zsh/zshrc" ]; then
+        if [ -f "$file" ]; then
+            ln -s $file $ZSH_CONFIG_DIR
+        fi
+    fi
+done
