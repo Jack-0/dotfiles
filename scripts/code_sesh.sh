a44492e9880d053ac2b3f9d99b7b4e6cb8fdffd7
Jack <github.ay8yf@slmail.me>
2023-10-22 19:54:34 +0100

    improved code session scripts

--- scripts/code_sesh.sh
+++ scripts/code_sesh.sh
@@ -4,15 +4,18 @@
 # THIS SCRIPT ASSUMES YOU STORE ALL YOUR WORK CODE IN ONE DIR
 #
 # 1. brew install fzf
-# 2. change ~/dev to where ever you store your git repos
+# 2. alter the dirs variable
+dirs=("$HOME/work/x" "$HOME/work/y")
+
+# fuzzy find selected
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/dev -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find "${dirs[@]}" -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
-
+# if nothing is selected exit
 if [[ -z $selected ]]; then
     exit 0
 fi
-
+# open with vs-code
 code $selected
--- scripts/sesh.sh
+++ scripts/sesh.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
+# sesh.sh
+# Create a tmux session for a directory. Looks for sub-dirs from the parent directory
+#
 # Requirements
 #
 # THIS SCRIPT ASSUMES YOU STORE ALL YOUR WORK CODE IN ONE DIR
 # IT ALSO ASSUMES YOU ARE RUNNING TMUX
 #
 # 1. brew install fzf
-# 2. change ~/dev to where ever you store your git repos
+# 2. change 'dirs' to directories you would like to fuzzy filter
+dirs=("$HOME/pdev" "$HOME/dev" "$HOME/learning")
+
+# fuzzy select session
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/pdev ~/dev ~/learning -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find "${dirs[@]}" -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
-
+selected_name=$(basename "$selected" | tr . _)
+# fail if no selection
 if [[ -z $selected ]]; then
     exit 0
 fi
-
-selected_name=$(basename "$selected" | tr . _)
+# handle the edge case that we are not currently in tmux
 tmux_running=$(pgrep tmux)
-
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-    echo "new session"
     tmux new-session -s $selected_name -c $selected
     exit 0
 fi
-
+# create session if it doesn't exist and in tmux
 if ! tmux has-session -t=$selected_name 2> /dev/null; then
-    echo "new session -ds"
     tmux new-session -ds $selected_name -c $selected
 fi
-
-echo "session switch"
-echo $selected_name
+# attach to session
 tmux switch-client -t $selected_name
