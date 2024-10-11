d7443e52e4c9cd7654bfde738db891489a22446c
Jack <github.ay8yf@slmail.me>
2024-10-11 23:18:12 +0100

    use toml config for ssh

--- scripts/ssh.py
+++ scripts/ssh.py
@@ -1,32 +1,33 @@
 #!/usr/bin/env python3
+import subprocess
+import os
+import tomllib
+from pathlib import Path
 # sesh.sh
 # Create a tmux session for ssh
 #
 # Requirements
 # 1. brew install fzf
-# 2. change 'sshHostCmd' as required
-sshHostsCmd = {
-    "host1": "ssh user1@host1",
-    "host2": "ssh user2@host2",
-    "todo": "change me",
-}
+# 2. alter config.toml (you may need to "cp config.toml.example config.toml")
 
-import subprocess
-import os
 
 def inTmux():
     return "TMUX" in os.environ
 
+
 def tmuxSeshExists(sessionName):
-    return subprocess.run(['tmux', 'has-session', '-t', sessionName]).returncode == 0
+    return subprocess.run(
+        ['tmux', 'has-session', '-t', sessionName]
+    ).returncode == 0
 
 
 def tmuxAttach(sessionName):
     if inTmux():
         return subprocess.run(['tmux', 'switch-client', '-t', sessionName])
-    else:    
+    else:
         return subprocess.run(['tmux', 'attach-session', '-t', sessionName])
- 
+
+
 def createSession(name, cmd):
     subprocess.run(['tmux', 'new-session', '-d', '-s', name, cmd])
     tmuxAttach(name)
@@ -38,16 +39,35 @@ def startTmuxSSHSession(name, cmd):
     else:
         createSession(name, cmd)
 
+
 if __name__ == "__main__":
+    # current_dir = Path(__file__).parent
+    current_dir = Path(__file__).resolve().parent
+    config_file_path = current_dir.name + "/../config.toml"
+    print("conf path=", config_file_path)
+    with open(config_file_path, "rb") as f:
+        data = tomllib.load(f)
+    sshHostInfo = data["ssh"]
+
     # get seesion
-    names = list(sshHostsCmd)
+    names = [entry["name"] for entry in sshHostInfo]
     namesStr = '\n'.join([''.join(n) for n in names])
 
-    # pipe into fzf and get selected 
-    echo = subprocess.run(['echo', namesStr], stdout=subprocess.PIPE, text=True)
+    # pipe into fzf and get selected
+    echo = subprocess.run(['echo', namesStr],
+                          stdout=subprocess.PIPE, text=True)
     output = echo.stdout
-    fzf = subprocess.run(['fzf'], input=output, stdout=subprocess.PIPE, text=True)
-    selected = fzf.stdout
-    selected = selected.strip()
+    fzf = subprocess.run(['fzf'], input=output,
+                         stdout=subprocess.PIPE, text=True)
+    selected_name = fzf.stdout
+    selected_name = selected_name.strip()
 
-    startTmuxSSHSession(selected, sshHostsCmd[selected])
+    # find next entry with selected name
+    selected_ssh_info = next(
+        (entry for entry in sshHostInfo
+            if entry["name"].strip() == selected_name), None)
+
+    if selected_ssh_info is None:
+        raise Exception(selected_name, "Not found in ssh hosts list")
+
+    startTmuxSSHSession(selected_name, selected_ssh_info["cmd"])
